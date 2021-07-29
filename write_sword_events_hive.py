79a33585c73b53b00edba1c64059644ba3e3cfb0
ruthashford <ruth.ashford21@gmail.com>
2021-07-29 17:33:38 +0000

    adding presto and hive in docker

---
+++ write_sword_events_hive.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""Extract events from kafka and write them to hdfs
+"""
+import json
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import udf, from_json
+from pyspark.sql.types import StructType, StructField, StringType
+
+
+def purchase_sword_event_schema():
+    """
+    root
+    |-- Accept: string (nullable = true)
+    |-- Host: string (nullable = true)
+    |-- User-Agent: string (nullable = true)
+    |-- event_type: string (nullable = true)
+    |-- timestamp: string (nullable = true)
+    |-- color: string (nullable = true)
+    |-- quantity: string (nullable = true)
+
+    """
+    return StructType([
+        StructField("Accept", StringType(), True),
+        StructField("Host", StringType(), True),
+        StructField("User-Agent", StringType(), True),
+        StructField("event_type", StringType(), True),
+        StructField("color", StringType(), True),
+        StructField("quantity", StringType(), True),
+    ])
+
+
+@udf('boolean')
+def is_sword_purchase(event_as_json):
+    """udf for filtering events
+    """
+    event = json.loads(event_as_json)
+    if event['event_type'] == 'purchase_sword':
+        return True
+    return False
+
+
+def main():
+    """main
+    """
+    spark = SparkSession \
+        .builder \
+        .appName("ExtractEventsJob") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+    raw_events = spark \
+        .read \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka:29092") \
+        .option("subscribe", "events") \
+        .option("startingOffsets", "earliest") \
+        .option("endingOffsets", "latest") \
+        .load()
+    
+    sword_events = raw_events \
+        .select(raw_events.value.cast('string').alias('raw'),
+                raw_events.timestamp.cast('string')) \
+        .filter(is_sword_purchase('raw'))
+
+    extracted_sword_events = sword_events \
+        .rdd \
+        .map(lambda r: Row(timestamp=r.timestamp, **json.loads(r.raw))) \
+        .toDF()
+    extracted_sword_events.printSchema()
+    extracted_sword_events.show()
+
+    extracted_sword_events.registerTempTable("extracted_sword_events")
+
+    spark.sql("""
+        create external table if not exists sword_events1
+        stored as parquet
+        location '/tmp/sword_events1'
+        as
+        select * from extracted_sword_events
+    """)    
+
+
+if __name__ == "__main__":
+    main()
